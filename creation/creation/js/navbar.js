f40ad333600fded313d6d2d16f1677911af8b23e
zigeecust <zigeecust@163.com>
2020-10-06 17:52:45 +0800

    Site updated: 2020-10-06 17:52:36

--- creation/creation/js/navbar.js
+++ creation/creation/js/navbar.js
@@ -1,16 +1,16 @@
 // (function ($) {
 $(function () {
   // Mobile nav
-  var $content = $('.carousel'),           // 获取正文
-    $sidebar = $('.sidebar'),             // 获取边栏
-    isMobileNavAnim = false,              // 动画执行状态位
-    mobileNavAnimDuration = 200;          // 设定动画时间
+  var $content = $('.carousel'), // 获取正文
+    $sidebar = $('.sidebar'), // 获取边栏
+    isMobileNavAnim = false, // 动画执行状态位
+    mobileNavAnimDuration = 200; // 设定动画时间
 
-  var startMobileNavAnim = function () {  // 指定状态位
+  var startMobileNavAnim = function () { // 指定状态位
     isMobileNavAnim = true;
   };
 
-  var stopMobileNavAnim = function () {   // 指定时间后清除状态位
+  var stopMobileNavAnim = function () { // 指定时间后清除状态位
     setTimeout(function () {
       isMobileNavAnim = false;
     }, mobileNavAnimDuration);
@@ -18,15 +18,15 @@ $(function () {
 
   /* 边框栏缩进/显示按钮 */
   $('.navbar-toggle').on('click', function () {
-    if (isMobileNavAnim) return;          // 如果正处于动画中，不重复执行
-    startMobileNavAnim();                 // 状态位：动画开始
+    if (isMobileNavAnim) return; // 如果正处于动画中，不重复执行
+    startMobileNavAnim(); // 状态位：动画开始
     $content.toggleClass('on');
     $sidebar.toggleClass('on');
-    stopMobileNavAnim();                  // 动画结束（超时）后设定状态位：动画结束
+    stopMobileNavAnim(); // 动画结束（超时）后设定状态位：动画结束
   });
 
   /* 点击正文 去除边栏 */
-  $($content).on('click', function () {   
+  $($content).on('click', function () {
     if (isMobileNavAnim || !$content.hasClass('on')) return;
     $content.removeClass('on');
     $sidebar.removeClass('on');
@@ -37,14 +37,6 @@ $(function () {
     $content.removeClass('on');
     $sidebar.removeClass('on');
 
-};
-
-  /* 默认显示边栏 */
-  $(window).bind("load", function () {
-  if (window.matchMedia("(min-width: 768px)").matches) {
-    $content.addClass('on');
-    $sidebar.addClass('on');
-  }
-});
-
+  };
+  
 })(jQuery);
